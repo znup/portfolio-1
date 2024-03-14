9fe48a44148a319dae0d48da6d97e98749805b44
SirAdan <lpelcastre@bomuca.com>
2024-03-14 10:37:03 -0700

    [Add] dark theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+      },
+      textColor: {
+        default: 'var(--color-text)',
+        offset: 'var(--color-text-offset)',
+      },
+      backgroundColor: {
+        default: 'var(--color-background)',
+        offset: 'var(--color-background-offset)',
+      },
+      borderColor: {
+        default: 'var(--color-border)',
+      },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require('tailwindcss-fluid-type')],
 };
